306c148bb4493d6d99fbf06e9cbcc2052cd2d520
DinaraAbuseva <ijk9du8@gmail.com>
2024-10-31 09:41:57 +0300

    change

--- visual.py
+++ visual.py
@@ -4,8 +4,8 @@ from key_generate import key
 
 window = tk.Tk()
 window.title("Keygen")
-file_png = tk.PhotoImage(file="./Hitman.png")
-bg = tk.Label(image=file_png)
+file_png = tk.PhotoImage(file = "./Hitman.png")
+bg = tk.Label(image = file_png)
 
 mixer.init()
 mixer.music.load("music.mp3")
@@ -19,23 +19,25 @@ def play():
         mixer.music.pause()
 
 
-button_mus = tk.Button(window, text="Play!",command=play)
-button_mus.place(in_=bg, relx=0, rely=0, anchor="nw", width=80, height=40)
+button_mus = tk.Button(window, text = "Play!",command = play)
+button_mus.place(in_ = bg, relx = 0, rely = 0, 
+                 anchor = "nw", 
+                 width = 80, height = 40)
 
-button_gen = tk.Button(window, text="Generate!", command=key)
+button_gen = tk.Button(window, text = "Generate!", command = key)
 
 
 def animation(i,j):
-    if i<500:
-        if j<300:
-            button_gen.place(x=i,y=j)
+    if i < 500:
+        if j < 300:
+            button_gen.place(x = i,y = j)
             button_gen.after(50,lambda: animation(i,j))
-            j+=1
-        i+=2
+            j += 1
+        i += 2
 
 animation(0,0)
 
-bg.pack(fill="both", expand="YES")
+bg.pack(fill = "both", expand = "YES")
 
 
 
